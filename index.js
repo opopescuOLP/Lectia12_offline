0b8d5110022c3cc2ef2a4a50b7dd00ed53e13f78
opopescuOLP <reprezinta@gmail.com>
2023-09-25 18:34:15 +0300

    second commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// const str ="un string";
+// const number = 9;
+// const bool = true;
+// const symb = Symbol("foo");
+// const bigInt = BigInt(9999999999999999999);
+// const unde = undefined;
+// const nullisValue = null;
+
+// const obj = {};
+// conts arr = [];
+// console.log(typeof symb)
+
+// const str1 = 'Hello'
+// const str2 = 'World'
+
+// console.log(`${str1} ${str2}    text orice ${str1}`)
+
+// const obj = {n: 'N'
+// p:'I'}
+// // 
+
+// console.log (obj) 
+
+// const condition = true;
+// const falsy1 = 0;
+// const falsy2 = '';
+// const falsy3 = undefined;
+// const falsy4 = null;
+// const falsy5 = NaN;
+
+// if (falsy5) { console.log('pas') }
+// else { console.log('do not pas') } 
+
+// console.log(2=='2');
+// console.log(2==='2');
+
+// console.log(!true);
+// console.log(!(1>2));
+
+const cond1 = 0;
+const cond2 = 1;
+
+
+if (cond1 || cond2 ) { console.log('pas') }
+// else { console.log('do not pas') }
